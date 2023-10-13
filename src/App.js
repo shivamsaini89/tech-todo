2ad06a070f644499b11e5818b96b4f62436ac880
shivamsaini89 <sainishivam221@gmail.com>
2023-10-13 23:03:41 +0530

    more function implemented

--- src/App.js
+++ src/App.js
@@ -5,16 +5,37 @@ import TodoList from './TodoList';
 function App() {
   const [list, setList] = useState([]);
   const [input, setInput] = useState("");
+  const [time, setTime] = useState([]);
 
   const handleChange = (e) => {
     setInput(e.target.value);
   }
 
   const handleClick = () => {
+    if (input === "") {
+      alert("Please add items to the list");
+    }
+    else {
+      setList((prevItems) => {
+        return [...prevItems, input];
+      });
+      setInput("");
+      setTime((prevTime) => {
+        return [...prevTime, new Date()];
+      });
+    }
+  };
+
+  const deleteItem = (id) => {
+    console.log("item deleted")
     setList((prevItems) => {
-      return  [...prevItems, input]
-    });
+      return (prevItems.filter((arr, index) => {
+        return index !== id
+      })
+      )
+    })
   }
+
 
   return (
     <>
@@ -23,11 +44,19 @@ function App() {
           <br />
           <h1>TODO List</h1>
           <br />
-          <input placeholder='Add TODO Items' onChange={handleChange} />
+          <input placeholder='Add TODO Items' onChange={handleChange} value={input} />
           <button onClick={handleClick}>Add</button>
           <ol>
-            {list.map((input, index, currentTime) => {
-              return <TodoList key={index} input={input} />
+            {list.map((time, index ) => {
+              return (
+                <TodoList
+                  key={index}
+                  id={index}
+                  input={input}
+                  time={time}
+                  onDelete={deleteItem}
+                   />
+              );
             })}
           </ol>
         </div>
--- src/TodoList.js
+++ src/TodoList.js
@@ -1,14 +1,21 @@
 import React from 'react'
 import './App.css';
 
-const Todo_list=(props)=> {
-  return (
-    <>
-     <div>
-        <li>{props.input}</li>
-    </div> 
-    </>
-  )
+const Todo_list = (props) => {
+    
+    console.log(props.time)
+    return (
+        <>
+            <div className="list-style">
+                <i className="fa fa-trash" aria-hidden="true" 
+                    onClick={()=>{
+                        props.onDelete(props.id)
+                }}>X</i>
+                <li>{props.input} </li>
+                <h6>{props.time}</h6>
+            </div>
+        </>
+    );
 }
 
 export default Todo_list
